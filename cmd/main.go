3e442c9df47a0795e8f9f4b44012b3fee5a38fff
z3phyrushka <ilyapolevikk@gmail.com>
2025-09-10 21:30:14 +0800

    1st commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "morse-converter ", log.LstdFlags|log.Lshortfile)
+
+	srv := server.NewServer(logger)
+
+	logger.Println("Сервер запущен на http://localhost:8080")
+	if err := srv.HTTP.ListenAndServe(); err != nil {
+		logger.Fatal(err)
+	}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,21 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func AutoConvert(input string) (string, error) {
+	input = strings.TrimSpace(input)
+	if input == "" {
+		return "", errors.New("пустой ввод")
+	}
+
+	if strings.ContainsAny(input, ".-/") {
+		return morse.ToText(input), nil
+	}
+
+	return morse.ToMorse(input), nil
+}
